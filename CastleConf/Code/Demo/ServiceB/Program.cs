61b20900220f2a1d3949861122bfa6101cd6bd21
Roger Johansson <roger@asynkron.se>
2024-04-16 15:56:45 +0200

    .

--- CastleConf/Code/Demo/ServiceB/Program.cs
+++ CastleConf/Code/Demo/ServiceB/Program.cs
@@ -1,3 +1,9 @@
+using System.Diagnostics;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -15,6 +21,50 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+
+//this defines your resource, meaning the service name, and various associated attribs
+var b = ResourceBuilder.CreateDefault();
+b.AddService("ServiceB");
+
+//this is used to create manual spans
+var activitySource = new ActivitySource("some-name");
+
+//configure logging to push to Otel
+builder.Services.AddLogging(loggingBuilder =>
+{
+    loggingBuilder.AddOpenTelemetry(o =>
+    {
+        o
+            .SetResourceBuilder(b)
+            .AddOtlpExporter();
+    });
+});
+
+//add Otel services
+builder.Services.AddOpenTelemetry()
+    //configure metrics
+    .WithMetrics(meterProviderBuilder =>
+        {
+            meterProviderBuilder
+                .AddAspNetCoreInstrumentation()
+                .AddHttpClientInstrumentation()
+                .SetResourceBuilder(b)
+                .AddOtlpExporter();
+        }
+    )
+    //configure tracing
+    .WithTracing(tracerProviderBuilder =>
+        {
+            tracerProviderBuilder
+                .AddAspNetCoreInstrumentation()
+                .AddHttpClientInstrumentation()
+                .AddSource(activitySource.Name)
+                .SetResourceBuilder(b)
+                .AddOtlpExporter();
+        }
+    );
+
 
 var summaries = new[]
 {
